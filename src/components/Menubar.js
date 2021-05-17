b31ae0f34aa802dfdc8962f982420618d7ac2519
Bayathy <misosoup0321@icloud.com>
2021-05-18 05:48:54 +0900

    今から怖いことをします

---
+++ src/components/Menubar.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+
+import "./menubar.css"
+
+const Menubar = () =>(
+    <div className="menubar">
+        <p>Home</p>
+        <p>Works</p>
+        <p>History</p>
+    </div>
+)
+
+export default Menubar
