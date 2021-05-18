7264b45d5aa03736b51eb241eaa1524aa4d12cb7
Bayathy <misosoup0321@icloud.com>
2021-05-19 01:15:34 +0900

    Worksまで終わった

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,12 +1,13 @@
 import * as React from "react";
 import WorkCard from "./Workcard";
 
+import Workimg from "./works-portfolio.png"
 import "./Work.css"
 
 const Work = ()=>(
     <div class="works-back">
-        <div><p className="Work">Work</p></div>
-        <WorkCard className="WorkCard" file={""} text={"このサイト"}/>
+        <div><p className="Work">Works</p></div>
+        <WorkCard className="WorkCard" file={Workimg} text={"このサイト"}/>
     </div>
 )
 
